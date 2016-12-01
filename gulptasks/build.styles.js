54fc7895067c00e0d979c4b005f0ce1235d0f0b1
Alan Sanchez <sanchezpineda03@gmail.com>
2016-12-01 14:03:50 -0600

    Created js and stylus gulptasks.

---
+++ gulptasks/build.styles.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp'),
+  config = require('../gulpfile.config'),
+  stylus = require('gulp-stylus'),
+  autoprefixer = require('autoprefixer-stylus')
+  import_tree = require('stylus-import-tree'),
+  cssmin = require('gulp-minify-css');
+
+gulp.task('build:styles', function () {
+  gulp.src(config.paths.src.stylMain)
+  .pipe(stylus({
+    use: autoprefixer('last 2 versions'),
+    define: {
+      import_tree: import_tree
+    }
+  }))
+  .pipe(cssmin({processImport: false}))
+  .pipe(gulp.dest(config.paths.dist.css));
+});
