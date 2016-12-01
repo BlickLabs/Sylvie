4a188096c310911715319d89144b0008a03f0ba6
Alan Sanchez <sanchezpineda03@gmail.com>
2016-12-01 13:54:52 -0600

    Gulp configfile setup.

---
+++ gulpfile.config.js
@@ -0,0 +1,36 @@
+var src = './src',
+  dist = './dist',
+  bower = './bower_components',
+  package = require('./package.json').name;
+
+module.exports = {
+  paths: {
+    src: {
+      styl: src + '/styl/**/*.styl',
+      stylMain: src + '/styl/main.styl',
+      js: src + '/js/**/*.js',
+      img: src + '/img/**/*.*',
+      favicon: src + '/favicon.ico',
+      fonts: src + '/fonts/**/*.*',
+      root: src
+    },
+    dist: {
+      css: dist + '/css/',
+      js: dist + '/js/',
+      img: dist + '/img/',
+      fonts: dist + '/fonts/',
+      root: dist 
+    },
+    bower: function (package) {
+      return bower + '/' + package;
+    }
+  },
+  outputs: {
+    libs: function (extension) {
+      return package + '.libs.' + extension; 
+    },
+    files: function (extension) {
+      return package + '.' + extension; 
+    }
+  }
+};
