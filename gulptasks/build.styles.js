4458080892c7981360db78fc2268e4713209d102
Alan Sanchez <sanchezpineda03@gmail.com>
2016-12-01 14:49:56 -0600

    Added bootstrap-styl to stylus gulptask.

--- gulptasks/build.styles.js
+++ gulptasks/build.styles.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
   config = require('../gulpfile.config'),
   stylus = require('gulp-stylus'),
+  bootstrap = require('bootstrap-styl'),
   autoprefixer = require('autoprefixer-stylus')
   import_tree = require('stylus-import-tree'),
   cssmin = require('gulp-minify-css');
@@ -8,7 +9,7 @@ var gulp = require('gulp'),
 gulp.task('build:styles', function () {
   gulp.src(config.paths.src.stylMain)
   .pipe(stylus({
-    use: autoprefixer('last 2 versions'),
+    use: [bootstrap(), autoprefixer('last 2 versions')],
     define: {
       import_tree: import_tree
     }
