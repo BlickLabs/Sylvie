a5688ae883c12b55e2ef2dd251764e78594e316b
Alan Sanchez <sanchezpineda03@gmail.com>
2016-12-09 00:20:42 -0600

    Added 'section' nunjucks variable on gulptask for html files.

--- gulptasks/build.html.js
+++ gulptasks/build.html.js
@@ -1,9 +1,16 @@
 var gulp = require('gulp'),
   config = require('../gulpfile.config'),
-  nunjucksRender = require('gulp-nunjucks-render');
+  nunjucksRender = require('gulp-nunjucks-render')
+  data = require('gulp-data');
 
 gulp.task('build:html', function () {
   gulp.src(config.paths.src.sectionTemplates)
+  .pipe(data(function (file) {
+    var sectionName = file.relative.split('.njk')[0];
+    return {
+      section: sectionName
+    }
+  }))
   .pipe(nunjucksRender({
     path: config.paths.src.templatesFolder,
     ext: '.html',
