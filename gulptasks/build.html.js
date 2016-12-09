d748f5452616f376be451585f1dc0215c72853d6
Alan Sanchez <sanchezpineda03@gmail.com>
2016-12-09 00:50:01 -0600

    Added gulp-data to node dependencies. Added global variable that refers to node package name.

--- gulptasks/build.html.js
+++ gulptasks/build.html.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
   config = require('../gulpfile.config'),
   nunjucksRender = require('gulp-nunjucks-render')
-  data = require('gulp-data');
+  data = require('gulp-data'),
+  package = require('../package.json').name;
 
 gulp.task('build:html', function () {
   gulp.src(config.paths.src.sectionTemplates)
@@ -14,7 +15,10 @@ gulp.task('build:html', function () {
   .pipe(nunjucksRender({
     path: config.paths.src.templatesFolder,
     ext: '.html',
-    inheritExtension: false
+    inheritExtension: false,
+    manageEnv: function (environment) {
+      environment.addGlobal('projectName', package);
+    }
   }))
   .pipe(gulp.dest(config.paths.dist.root));
 });
